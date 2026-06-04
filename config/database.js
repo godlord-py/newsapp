7afb47cdf7b1c7931313dd0e6a2eaaba2ca301f5
aayushfireblaze <aayush.fireblaze@gmail.com>
2026-06-04 19:17:29 +0530

    idk

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 import { Sequelize } from 'sequelize';
 
 // Replace with your actual Render database connection details
-const sequelize = new Sequelize('news_db_make_user', 'news_db_make_user', 'Tb4ZlFY7JbKQOGQ6RCYEcSV19pBehKxo', {
+const sequelize = new Sequelize('news_db_make', 'news_db_make_user', 'Tb4ZlFY7JbKQOGQ6RCYEcSV19pBehKxo', {
   host: 'singapore-postgres.render.com',
   dialect: 'postgres',
   port: 5432, 
